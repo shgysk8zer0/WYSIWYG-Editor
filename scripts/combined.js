32eefeedb9bd95f61890edc42faff6842ac503f5
Chris Zuber <shgysk8zer0@gmail.com>
2015-06-17 09:49:37 -0700

    :arrow_up: std-js @467e282
    Submodule scripts/std-js a2aa402..467e282:
    > Fix missing ctrlKey

--- scripts/combined.js
+++ scripts/combined.js
@@ -1243,56 +1243,56 @@ window.addEventListener('keypress', function (event) {
     if (event.target.matches('[contenteditable="true"], [contenteditable="true"] *')) {
         switch (event.key.toLowerCase()) {
             case 'y':
-                if (!(event.altKey || event.shiftKey)) {
+                if (event.ctrlKey && !(event.altKey || event.shiftKey)) {
                     event.preventDefault();
                     event.stopPropagation();
                     document.execCommand('redo');
                 }
                 break;
             case 'z':
-                if (!(event.altKey || event.shiftKey)) {
+                if (event.ctrlKey && !(event.altKey || event.shiftKey)) {
                     event.preventDefault()
                     event.stopPropagation();
                     document.execCommand('undo');
                 }
                 break;
             case 'a':
-                if (!(event.altKey || event.shiftKey)) {
+                if (event.ctrlKey && !(event.altKey || event.shiftKey)) {
                     event.preventDefault();
                     event.stopPropagation();
                     document.execCommand('selectall');
                 }
                 break;
             case 'e':
-                if (!(event.altKey || event.shiftKey)) {
+                if (event.ctrlKey && !(event.altKey || event.shiftKey)) {
                     event.preventDefault();
                     event.stopPropagation();
                     document.execCommand('justifyCenter');
                 }
                 break;
             case 'l':
-                if (!(event.altKey || event.shiftKey)) {
+                if (event.ctrlKey && !(event.altKey || event.shiftKey)) {
                     event.preventDefault();
                     event.stopPropagation();
                     document.execCommand('justifyLeft');
                 }
                 break;
             case 'r':
-                if (!(event.altKey || event.shiftKey)) {
+                if (event.ctrlKey && !(event.altKey || event.shiftKey)) {
                     event.preventDefault();
                     event.stopPropagation();
                     document.execCommand('justifyRight');
                 }
                 break;
             case 'j':
-                if (!(event.altKey || event.shiftKey)) {
+                if (event.ctrlKey && !(event.altKey || event.shiftKey)) {
                     event.preventDefault();
                     event.stopPropagation();
                     document.execCommand('justiyFull');
                 }
                 break;
             case 'i':
-                if (!(event.altKey || event.shiftKey)) {
+                if (event.ctrlKey && !(event.altKey || event.shiftKey)) {
                     event.preventDefault();
                     event.stopPropagation();
                     document.execCommand('styleWithCSS', null, false);
@@ -1300,7 +1300,7 @@ window.addEventListener('keypress', function (event) {
                 }
                 break;
             case 'b':
-                if (!(event.altKey || event.shiftKey)) {
+                if (event.ctrlKey && !(event.altKey || event.shiftKey)) {
                     event.preventDefault();
                     event.stopPropagation();
                     document.execCommand('styleWithCSS', null, false);
@@ -1308,7 +1308,7 @@ window.addEventListener('keypress', function (event) {
                 }
                 break;
             case 'u':
-                if (!(event.altKey || event.shiftKey)) {
+                if (event.ctrlKey && !(event.altKey || event.shiftKey)) {
                     event.preventDefault();
                     event.stopPropagation();
                     document.execCommand('styleWithCSS', null, false);
@@ -1316,7 +1316,7 @@ window.addEventListener('keypress', function (event) {
                 }
                 break;
             case 'k':
-                if (!(event.altKey || event.shiftKey)) {
+                if (event.ctrlKey && !(event.altKey || event.shiftKey)) {
                     event.preventDefault();
                     event.stopPropagation();
                     document.execCommand('styleWithCSS', null, false);
